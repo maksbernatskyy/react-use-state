f9019283db52774d69668989ccb62e8807f9a3c9
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-07 12:19:44 +0100

    add list of buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <>
         <header className="bg-body-secondary">
-            <h1 className="p-2">
+            <h1 className="p-4">
                 Learn Web development
             </h1>
         </header>
