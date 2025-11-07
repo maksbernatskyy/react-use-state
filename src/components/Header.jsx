b2fe975cb57fc813f2320563d74b7784af496fac
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-07 11:49:18 +0100

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ export default function Header() {
 
     return (
         <>
-        
+        <header className="bg-body-secondary">
+            <h1 className="p-2">
+                Learn Web development
+            </h1>
+        </header>
         </>
     )
 }
